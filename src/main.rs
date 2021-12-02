e95f6fb4c56c78a17ac7e5939768a332b4894ad6
Demian Ferreiro <epidemian@gmail.com>
2021-12-02 00:56:36 -0300

    Extract function to count increments and remove duplication

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,23 @@
 use std::fs;
 
 fn main() {
-    // Part 1
     let input = fs::read_to_string("inputs/day01").unwrap();
     let numbers: Vec<i32> = input.lines().map(|l| l.parse().unwrap()).collect();
+    println!("{}", count_increments(&numbers));
 
-    let mut last_num = 0;
-    let mut increase_count = -1; // First number shouldn't count.
-    for &num in numbers.iter() {
-        if num > last_num {
-            increase_count += 1;
-        }
-        last_num = num;
-    }
-    println!("{}", increase_count);
-
-    // Part 2
     let mut sums = vec![];
     for i in 0..numbers.len() - 2 {
         sums.push(numbers[i] + numbers[i + 1] + numbers[i + 2]);
     }
+    println!("{}", count_increments(&sums));
+}
 
-    let mut last_num = 0;
-    let mut increase_count = -1; // First number shouldn't count.
-    for &num in sums.iter() {
-        if num > last_num {
-            increase_count += 1;
+fn count_increments(numbers: &Vec<i32>) -> i32 {
+    let mut result = 0;
+    for (i, &num) in numbers.iter().enumerate() {
+        if i > 0 && num > numbers[i - 1] {
+            result += 1;
         }
-        last_num = num;
     }
-
-    println!("{}", increase_count);
+    return result;
 }
