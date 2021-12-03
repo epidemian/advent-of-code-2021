da7d9710838be229d97506c9360a7f8345e3f38d
Demian Ferreiro <epidemian@gmail.com>
2021-12-03 13:41:47 -0300

    Make day 2 solution it terser
    Learned about split_once(), which helped simplify the parsing logic a
    lot. Also removed the enum and let the parse function return just
    strings; makes things less complicated.

--- src/day02.rs
+++ src/day02.rs
@@ -8,9 +8,10 @@ fn part1() {
   let mut depth = 0;
   for (command, value) in parse_commands() {
     match command {
-      Command::Forward => pos += value,
-      Command::Down => depth += value,
-      Command::Up => depth -= value,
+      "forward" => pos += value,
+      "down" => depth += value,
+      "up" => depth -= value,
+      _ => unreachable!(),
     }
   }
   println!("{}", pos * depth)
@@ -22,36 +23,24 @@ fn part2() {
   let mut aim = 0;
   for (command, value) in parse_commands() {
     match command {
-      Command::Forward => {
+      "forward" => {
         pos += value;
         depth += aim * value
       }
-      Command::Down => aim += value,
-      Command::Up => aim -= value,
+      "down" => aim += value,
+      "up" => aim -= value,
+      _ => unreachable!(),
     }
   }
   println!("{}", pos * depth)
 }
 
-enum Command {
-  Forward,
-  Down,
-  Up,
-}
-
-fn parse_commands() -> Vec<(Command, i32)> {
+fn parse_commands() -> Vec<(&'static str, i32)> {
   include_str!("inputs/day02")
     .lines()
-    .map(|line| {
-      let mut parts = line.split_whitespace();
-      let command = parts.next().unwrap();
-      let value: i32 = parts.next().unwrap().parse().unwrap();
-      match command {
-        "forward" => (Command::Forward, value),
-        "down" => (Command::Down, value),
-        "up" => (Command::Up, value),
-        _ => panic!("unknown command {}", command),
-      }
+    .map(|line: &str| {
+      let (command, value) = line.split_once(" ").unwrap();
+      (command, value.parse().unwrap())
     })
     .collect()
 }
