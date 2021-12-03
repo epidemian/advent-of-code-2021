daba63d3a0b2ea291e6702ba6480ee4dc6d824dc
Demian Ferreiro <epidemian@gmail.com>
2021-12-03 01:28:46 -0300

    Extract common parsing logic

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Trying to learn some Rust for [Advent of Code 2021](https://adventofcode.com/202
 ## Notes/learnings
 
 - Day 1: Simple challenge, but nice to get to know some iteration-related functions. The solution is probably not very idiomatic. I don't really understand how references or mutability work still.
+- Day 2: Learned about include_str!, std::env::args(), enums, and matching on strings. Still, i'm using .unwrap() with no abandon, slapping it anywhere that requires the slightest error handling. Parsing logic could surely be improved a lot.
--- src/day02.rs
+++ src/day02.rs
@@ -4,41 +4,54 @@ pub fn run() {
 }
 
 fn part1() {
-  let input = include_str!("inputs/day02");
   let mut pos = 0;
   let mut depth = 0;
-  for line in input.lines() {
-    let mut parts = line.split_whitespace();
-    let command = parts.next().unwrap();
-    let value: i32 = parts.next().unwrap().parse().unwrap();
+  for (command, value) in parse_commands() {
     match command {
-      "forward" => pos += value,
-      "down" => depth += value,
-      "up" => depth -= value,
-      _ => println!("unknown command {}", command),
+      Command::Forward => pos += value,
+      Command::Down => depth += value,
+      Command::Up => depth -= value,
     }
   }
   println!("{}", pos * depth)
 }
 
 fn part2() {
-  let input = include_str!("inputs/day02");
   let mut pos = 0;
   let mut depth = 0;
   let mut aim = 0;
-  for line in input.lines() {
-    let mut parts = line.split_whitespace();
-    let command = parts.next().unwrap();
-    let value: i32 = parts.next().unwrap().parse().unwrap();
+  for (command, value) in parse_commands() {
     match command {
-      "forward" => {
+      Command::Forward => {
         pos += value;
         depth += aim * value
       }
-      "down" => aim += value,
-      "up" => aim -= value,
-      _ => println!("unknown command {}", command),
+      Command::Down => aim += value,
+      Command::Up => aim -= value,
     }
   }
   println!("{}", pos * depth)
+}
+
+enum Command {
+  Forward,
+  Down,
+  Up,
+}
+
+fn parse_commands() -> Vec<(Command, i32)> {
+  include_str!("inputs/day02")
+    .lines()
+    .map(|line| {
+      let mut parts = line.split_whitespace();
+      let command = parts.next().unwrap();
+      let value: i32 = parts.next().unwrap().parse().unwrap();
+      match command {
+        "forward" => (Command::Forward, value),
+        "down" => (Command::Down, value),
+        "up" => (Command::Up, value),
+        _ => panic!("unknown command {}", command),
+      }
+    })
+    .collect()
 }
