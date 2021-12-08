f083ed007f9a685876a93d89e02536f17ef38591
Demian Ferreiro <epidemian@gmail.com>
2021-12-08 17:59:44 -0300

    Simplify day 5 solution
    * Use a tuple instead of a struct for lines
    * Make count_overlaps take a slice reference instead of a generic
      Iterator and all the mess that implies
    * Use HashMap instead of an array for the map

--- src/day05.rs
+++ src/day05.rs
@@ -1,52 +1,44 @@
-const SIZE: usize = 1000;
+use std::collections::HashMap;
 
 pub fn run() {
   let all_lines: Vec<Line> = include_str!("inputs/day05")
     .lines()
-    .map(Line::parse)
+    .map(parse_line)
     .collect();
-  let non_diagonal_lines = all_lines
+  let non_diagonal_lines: Vec<Line> = all_lines
     .iter()
-    .filter(|Line(x1, y1, x2, y2)| x1 == x2 || y1 == y2);
+    .filter(|(x1, y1, x2, y2)| x1 == x2 || y1 == y2)
+    .map(|l| l.clone())
+    .collect();
 
-  println!("{}", count_overlaps(non_diagonal_lines));
-  println!("{}", count_overlaps(all_lines.iter()));
+  println!("{}", count_overlaps(&non_diagonal_lines));
+  println!("{}", count_overlaps(&all_lines));
 }
 
-fn count_overlaps<'a, I>(lines: I) -> usize
-where
-  I: Iterator<Item = &'a Line>,
-{
-  let mut map = [[0; SIZE]; SIZE];
-  for &Line(x1, y1, x2, y2) in lines {
-    map[x1 as usize][y1 as usize] += 1;
-
+fn count_overlaps(lines: &[Line]) -> usize {
+  let mut counts = HashMap::new();
+  for &(x1, y1, x2, y2) in lines {
     let dx = (x2 - x1).signum();
     let dy = (y2 - y1).signum();
     let mut x = x1;
     let mut y = y1;
+    *counts.entry((x, y)).or_insert(0) += 1;
     while !(x == x2 && y == y2) {
       x += dx;
       y += dy;
-      map[x as usize][y as usize] += 1;
+      *counts.entry((x, y)).or_insert(0) += 1
     }
   }
-  map
-    .iter()
-    .map(|line| line.iter().filter(|&&cell| cell >= 2).count())
-    .sum()
+  counts.values().filter(|count| **count >= 2).count()
 }
 
-#[derive(Debug)]
-struct Line(i32, i32, i32, i32);
+type Line = (i32, i32, i32, i32);
 
-impl Line {
-  fn parse(s: &str) -> Line {
-    let points: Vec<i32> = s
-      .split(" -> ")
-      .flat_map(|p| p.split(","))
-      .map(|s| s.parse().unwrap())
-      .collect();
-    Line(points[0], points[1], points[2], points[3])
-  }
+fn parse_line(s: &str) -> Line {
+  let points: Vec<i32> = s
+    .split(" -> ")
+    .flat_map(|p| p.split(","))
+    .map(|s| s.parse().unwrap())
+    .collect();
+  (points[0], points[1], points[2], points[3])
 }
