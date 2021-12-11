15dfce8dd49fa61f5a519b3e6d47fcb1f74685e1
Demian Ferreiro <epidemian@gmail.com>
2021-12-11 11:45:54 -0300

    Use an array of functions instead of a match statement in main()
    One less number to repeat for each day :)

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,19 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 fn main() {
+    let days = [
+        day01::run,
+        day02::run,
+        day03::run,
+        day04::run,
+        day05::run,
+        day06::run,
+    ];
     let args: Vec<String> = env::args().collect();
-    let day_num = args.last().unwrap();
-    match day_num.as_ref() {
-        "1" => day01::run(),
-        "2" => day02::run(),
-        "3" => day03::run(),
-        "4" => day04::run(),
-        "5" => day05::run(),
-        _ => println!("invalid day number {}", day_num),
-    }
+    let day_num: usize = args.last().unwrap().parse().unwrap();
+
+    days[day_num - 1]()
 }
