2b250eac79d06eeba7773e3354fdeae795526fd4
Demian Ferreiro <epidemian@gmail.com>
2021-12-13 18:32:40 -0300

    Improve day 12 solution
    Generaliza count_paths() function to take a argument for the "can visit"
    criteria and remove duplication between part 1 and 2.

--- src/day12.rs
+++ src/day12.rs
@@ -8,44 +8,49 @@ pub fn run() {
     cave_graph.entry(cave_2).or_insert(vec![]).push(cave_1);
   }
 
-  println!("{}", count_paths(&cave_graph, "start", "end", &vec!()));
-  println!("{}", count_paths_2(&cave_graph, "start", "end", &vec!()));
+  println!("{}", part_1(&cave_graph));
+  println!("{}", part_2(&cave_graph));
 }
 
 type Graph = HashMap<&'static str, Vec<&'static str>>;
 
-fn count_paths(cave_graph: &Graph, from: &str, to: &str, visited_caves: &Vec<&str>) -> i32 {
-  if from == to {
+fn part_1(cave_graph: &Graph) -> i32 {
+  // Can visit small caves at most once.
+  let can_visit =
+    |cave: &str, visited_caves: &Vec<&str>| is_big(cave) || !visited_caves.contains(&cave);
+  count_paths(&cave_graph, "start", &vec![], can_visit)
+}
+
+fn part_2(cave_graph: &Graph) -> i32 {
+  // Can visit a single small cave twice.
+  let can_visit = |cave: &str, visited_caves: &Vec<&str>| {
+    is_big(cave)
+      || !visited_caves.contains(&cave)
+      || (cave != "start" && !small_cave_visited_twice(&visited_caves))
+  };
+  count_paths(&cave_graph, "start", &vec![], can_visit)
+}
+
+fn count_paths(
+  cave_graph: &Graph,
+  from: &str,
+  visited_caves: &Vec<&str>,
+  can_visit: fn(&str, &Vec<&str>) -> bool,
+) -> i32 {
+  if from == "end" {
     return 1;
   }
   let mut visited_caves = visited_caves.clone();
   visited_caves.push(from);
   cave_graph[from]
     .iter()
-    .filter(|cave| is_big(cave) || !visited_caves.contains(cave))
-    .map(|cave| count_paths(cave_graph, cave, to, &visited_caves))
+    .filter(|cave| can_visit(cave, &visited_caves))
+    .map(|cave| count_paths(cave_graph, cave, &visited_caves, can_visit))
     .sum()
 }
 
 fn is_big(cave: &str) -> bool {
   cave.chars().next().unwrap().is_ascii_uppercase()
-}
-
-fn count_paths_2(cave_graph: &Graph, from: &str, to: &str, visited_caves: &Vec<&str>) -> i32 {
-  if from == to {
-    return 1;
-  }
-  let mut visited_caves = visited_caves.clone();
-  visited_caves.push(from);
-  cave_graph[from]
-    .iter()
-    .filter(|&cave| {
-      is_big(cave)
-        || !visited_caves.contains(cave)
-        || (*cave != "start" && !small_cave_visited_twice(&visited_caves))
-    })
-    .map(|cave| count_paths_2(cave_graph, cave, to, &visited_caves))
-    .sum()
 }
 
 fn small_cave_visited_twice(visited_caves: &Vec<&str>) -> bool {
