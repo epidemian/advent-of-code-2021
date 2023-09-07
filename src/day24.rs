f103e325e9ad56adbfa775d40636325c44e2865b
Demian Ferreiro <epidemian@gmail.com>
2023-09-07 13:22:23 -0300

    Extract main model number logic into a separate function
    And call this function twice: once for the maximum model number, and
    once for the minimum.

--- src/day24.rs
+++ src/day24.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{cmp, collections::HashMap};
 
 pub fn run() {
     let program = parse_program(include_str!("inputs/day24"));
@@ -17,8 +17,18 @@ pub fn run() {
         }
     }
 
-    let mut max_inputs: HashMap<_, _> = HashMap::from_iter([(0, vec![])]);
-    let mut min_inputs: HashMap<_, _> = HashMap::from_iter([(0, vec![])]);
+    let max_model_number = find_model_number(&blocks, cmp::max);
+    let min_model_number = find_model_number(&blocks, cmp::min);
+
+    println!("{max_model_number}");
+    println!("{min_model_number}");
+}
+
+fn find_model_number<P>(blocks: &[Vec<Instruction>], pick_inputs: P) -> String
+where
+    P: Fn(Vec<i64>, Vec<i64>) -> Vec<i64>,
+{
+    let mut inputs_by_z: HashMap<_, _> = HashMap::from_iter([(0, vec![])]);
     for block in blocks.iter() {
         // Each block of this program behaves like this pseudo-code:
         //
@@ -52,63 +62,31 @@ pub fn run() {
             }
         }
 
-        let mut new_max_inputs = HashMap::new();
-        for (prev_z, prev_inputs) in max_inputs.iter() {
+        let mut new_inputs_by_z: HashMap<i64, Vec<i64>> = HashMap::new();
+        for (prev_z, prev_inputs) in inputs_by_z.iter() {
             for w in 1..=9 {
                 let [.., z] = run_alu_program(block, &[w], [0, 0, 0, *prev_z]);
                 if (can_reduce_z && z == *prev_z / 26) || !can_reduce_z {
                     let mut inputs = prev_inputs.clone();
                     inputs.push(w);
-                    if let Some(v) = new_max_inputs.get_mut(&z) {
-                        if &inputs > v {
-                            *v = inputs;
-                        }
+                    if let Some(v) = new_inputs_by_z.get_mut(&z) {
+                        *v = pick_inputs(v.clone(), inputs);
                     } else {
-                        new_max_inputs.insert(z, inputs);
+                        new_inputs_by_z.insert(z, inputs);
                     }
                 }
             }
         }
-        max_inputs = new_max_inputs;
-
-        let mut new_min_inputs = HashMap::new();
-        for (prev_z, prev_inputs) in min_inputs.iter() {
-            for w in 1..=9 {
-                let [.., z] = run_alu_program(block, &[w], [0, 0, 0, *prev_z]);
-                if (can_reduce_z && z == *prev_z / 26) || !can_reduce_z {
-                    let mut inputs = prev_inputs.clone();
-                    inputs.push(w);
-                    if let Some(v) = new_min_inputs.get_mut(&z) {
-                        if &inputs < v {
-                            *v = inputs;
-                        }
-                    } else {
-                        new_min_inputs.insert(z, inputs);
-                    }
-                }
-            }
-        }
-        min_inputs = new_min_inputs;
+        inputs_by_z = new_inputs_by_z;
     }
-    assert_eq!(max_inputs.len(), 1);
-    assert_eq!(min_inputs.len(), 1);
+    assert_eq!(inputs_by_z.len(), 1);
 
-    let max_inputs = max_inputs.values().next().unwrap();
-    let max_model_number = max_inputs
+    let inputs = inputs_by_z.values().next().unwrap();
+    inputs
         .iter()
         .map(|w| w.to_string())
         .collect::<Vec<_>>()
-        .join("");
-
-    let min_inputs = min_inputs.values().next().unwrap();
-    let min_model_number = min_inputs
-        .iter()
-        .map(|w| w.to_string())
-        .collect::<Vec<_>>()
-        .join("");
-
-    println!("{max_model_number}");
-    println!("{min_model_number}");
+        .join("")
 }
 
 fn run_alu_program(program: &[Instruction], inputs: &[i64], initial_vars: [i64; 4]) -> [i64; 4] {
