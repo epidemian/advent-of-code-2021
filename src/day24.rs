35c398a48a3526ef68b748fcc92eac9699f95544
Demian Ferreiro <epidemian@gmail.com>
2023-09-07 01:09:46 -0300

    Refactor day 24
    Use a generic loop over each "block" of the input program instead of the
    mess that it was before :)

--- src/day24.rs
+++ src/day24.rs
@@ -1,363 +1,92 @@
 use std::collections::HashMap;
 
 pub fn run() {
-    let input = "inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 1
-    add x 11
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 6
-    mul y x
-    add z y
-    inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 1
-    add x 13
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 14
-    mul y x
-    add z y
-    inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 1
-    add x 15
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 14
-    mul y x
-    add z y
-    inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 26
-    add x -8
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 10
-    mul y x
-    add z y";
-    let program = parse_program(input);
-    let mut zs = HashMap::new();
-    for w1 in 1..=9 {
-        for w2 in 1..=9 {
-            for w3 in 1..=9 {
-                for w4 in 1..=9 {
-                    let inputs = [w1, w2, w3, w4];
-                    let res = run_alu_program(&program, &inputs, [0, 0, 0, 0], false);
-                    if res[3] < 5000 {
-                        // println!("{w1}{w2}{w3}{w4} ->{res:?}");
-                        zs.insert(res[3], inputs);
-                    }
-                }
-            }
-        }
-    }
-    println!("{zs:?}");
-
-    let input2 = "inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 1
-    add x 13
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 9
-    mul y x
-    add z y
-    inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 1
-    add x 15
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 12
-    mul y x
-    add z y
-    inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 26
-    add x -11
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 8
-    mul y x
-    add z y";
-    let program2 = parse_program(input2);
-    let mut zs2 = HashMap::new();
-    for (z, z_inputs) in zs.iter() {
-        for w5 in 1..=9 {
-            for w6 in 1..=9 {
-                for w7 in 1..=9 {
-                    let inputs = [w5, w6, w7];
-                    let res = run_alu_program(&program2, &inputs, [0, 0, 0, *z], false);
-                    if res[3] < 50_000 {
-                        println!("{w5}{w6}{w7}, z={z} ->{res:?}");
-                        let [w1, w2, w3, w4] = *z_inputs;
-                        zs2.entry(res[3])
-                            .and_modify(|v: &mut [i64; 7]| {
-                                *v = [w1, w2, w3, w4, w5, w6, w7].max(*v)
-                            })
-                            .or_insert([w1, w2, w3, w4, w5, w6, w7]);
-                    }
-                }
-            }
+    let program = parse_program(include_str!("inputs/day24"));
+    let mut blocks = vec![];
+    for ins in program.into_iter() {
+        if matches!(ins, Instruction::Inp(_)) {
+            blocks.push(vec![ins]);
+        } else {
+            blocks
+                .last_mut()
+                .expect("a block of instructions should exist")
+                .push(ins);
         }
     }
 
-    let input3 = "inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 26
-    add x -4
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 13
-    mul y x
-    add z y
-    inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 26
-    add x -15
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 12
-    mul y x
-    add z y";
-    let program3 = parse_program(input3);
-    let mut zs3 = HashMap::new();
-    for (z, z_inputs) in zs2.iter() {
-        for w8 in 1..=9 {
-            for w9 in 1..=9 {
-                let res = run_alu_program(&program3, &[w8, w9], [0, 0, 0, *z], false);
-                if res[3] < 100 {
-                    println!("{w8}{w9}, z={z} ->{res:?}");
-                    let [w1, w2, w3, w4, w5, w6, w7] = *z_inputs;
-                    zs3.entry(res[3])
-                        .and_modify(|v: &mut [i64; 9]| {
-                            *v = [w1, w2, w3, w4, w5, w6, w7, w8, w9].max(*v)
-                        })
-                        .or_insert([w1, w2, w3, w4, w5, w6, w7, w8, w9]);
+    let mut max_inputs: HashMap<_, _> = HashMap::from_iter([(0, vec![])]);
+    let mut min_inputs: HashMap<_, _> = HashMap::from_iter([(0, vec![])]);
+    for block in blocks.iter() {
+        let mut can_reduce_z = false;
+        for z0 in 0..26 {
+            for w in 1..=9 {
+                let [.., z] = run_alu_program(block, &[w], [0, 0, 0, z0]);
+                if z == 0 {
+                    can_reduce_z = true;
                 }
             }
         }
-    }
 
-    let input4 = "inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 1
-    add x 14
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 6
-    mul y x
-    add z y
-    inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 1
-    add x 14
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 9
-    mul y x
-    add z y
-    inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 26
-    add x -1
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 15
-    mul y x
-    add z y
-    inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 26
-    add x -8
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 4
-    mul y x
-    add z y
-    inp w
-    mul x 0
-    add x z
-    mod x 26
-    div z 26
-    add x -14
-    eql x w
-    eql x 0
-    mul y 0
-    add y 25
-    mul y x
-    add y 1
-    mul z y
-    mul y 0
-    add y w
-    add y 10
-    mul y x
-    add z y";
-    let program4 = parse_program(input4);
-    let whole_program = parse_program(include_str!("inputs/day24"));
-    let mut zs4 = HashMap::new();
-    for (z, z_inputs) in zs3.iter() {
-        for w10 in 1..=9 {
-            for w11 in 1..=9 {
-                for w12 in 1..=9 {
-                    for w13 in 1..=9 {
-                        for w14 in 1..=9 {
-                            let res = run_alu_program(
-                                &program4,
-                                &[w10, w11, w12, w13, w14],
-                                [0, 0, 0, *z],
-                                false,
-                            );
-                            if res[3] == 0 {
-                                let [w1, w2, w3, w4, w5, w6, w7, w8, w9] = *z_inputs;
-                                let all_inputs =
-                                    [w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14];
-                                let res =
-                                    run_alu_program(&whole_program, &all_inputs, [0; 4], false);
-                                assert_eq!(res[3], 0);
-                                println!(
-                                    "{} (z={z}) is a valid model number, {res:?}!",
-                                    all_inputs.map(|w| w.to_string()).join("")
-                                );
-
-                                zs4.entry(res[3])
-                                    .and_modify(|v: &mut [i64; 14]| *v = (*v).max(all_inputs))
-                                    .or_insert(all_inputs);
-                            }
+        let mut new_max_inputs = HashMap::new();
+        for (prev_z, prev_inputs) in max_inputs.iter() {
+            for w in 1..=9 {
+                let [.., z] = run_alu_program(block, &[w], [0, 0, 0, *prev_z]);
+                if (can_reduce_z && z == *prev_z / 26) || !can_reduce_z {
+                    let mut inputs = prev_inputs.clone();
+                    inputs.push(w);
+                    if let Some(v) = new_max_inputs.get_mut(&z) {
+                        if &inputs > v {
+                            *v = inputs;
                         }
+                    } else {
+                        new_max_inputs.insert(z, inputs);
                     }
                 }
             }
         }
+        max_inputs = new_max_inputs;
+
+        let mut new_min_inputs = HashMap::new();
+        for (prev_z, prev_inputs) in min_inputs.iter() {
+            for w in 1..=9 {
+                let [.., z] = run_alu_program(block, &[w], [0, 0, 0, *prev_z]);
+                if (can_reduce_z && z == *prev_z / 26) || !can_reduce_z {
+                    let mut inputs = prev_inputs.clone();
+                    inputs.push(w);
+                    if let Some(v) = new_min_inputs.get_mut(&z) {
+                        if &inputs < v {
+                            *v = inputs;
+                        }
+                    } else {
+                        new_min_inputs.insert(z, inputs);
+                    }
+                }
+            }
+        }
+        min_inputs = new_min_inputs;
     }
-    println!("{zs4:?}");
+    assert_eq!(max_inputs.len(), 1);
+    assert_eq!(min_inputs.len(), 1);
+
+    let max_inputs = max_inputs.values().next().unwrap();
+    let max_model_number = max_inputs
+        .iter()
+        .map(|w| w.to_string())
+        .collect::<Vec<_>>()
+        .join("");
+
+    let min_inputs = min_inputs.values().next().unwrap();
+    let min_model_number = min_inputs
+        .iter()
+        .map(|w| w.to_string())
+        .collect::<Vec<_>>()
+        .join("");
+
+    println!("{max_model_number}");
+    println!("{min_model_number}");
 }
 
-fn run_alu_program(
-    program: &[Instruction],
-    inputs: &[i64],
-    initial_vars: [i64; 4],
-    debug: bool,
-) -> [i64; 4] {
+fn run_alu_program(program: &[Instruction], inputs: &[i64], initial_vars: [i64; 4]) -> [i64; 4] {
     let mut vars = initial_vars;
     let mut input = inputs.into_iter();
 
@@ -384,9 +113,6 @@ fn run_alu_program(
             Instruction::Eql(a, b) => {
                 vars[*a as usize] = (vars[*a as usize] == b.value(&vars)) as i64
             }
-        }
-        if debug {
-            println!("after {ins:?}: {vars:?}");
         }
     }
 
